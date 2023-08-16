e1590de0c090fcc2ac8913da269506d1c2d489e6
Devinajar <jarrhack@proton.me>
2023-08-16 12:15:46 +0200

    Changes to check.tf to fit the scenario

--- consolidate/check.tf
+++ consolidate/check.tf
@@ -1,61 +1,58 @@
 provider "google" {
-  credentials = file(var.admin_service_account_key)
   project     = var.project_id
-  region      = "us-central1"
+  region      = "europe-west1"
 }
 
+# Bucket creation
+### Service account that will load the data into the bucket
 resource "google_service_account" "data_loader" {
   account_id   = "data-loader-service-account"
   display_name = "Data Loader Service Account"
 }
 
+### Bind service account to bucket
 resource "google_project_iam_binding" "data_loader_binding" {
   project = var.project_id
-  role    = "roles/bigquery.dataEditor"
-  
+  role    = "roles/bigquery.dataEditor"  
   members = [
     "serviceAccount:${google_service_account.data_loader.email}"
   ]
 }
-
+### The bucket itself
 resource "google_storage_bucket" "csv_bucket" {
   name     = var.bucket_name
-  location = "us-central1"
-  versioning {
-    enabled = true
-  }
+  location = "europe-west2"
+  uniform_bucket_level_access = true
 }
 
+# Database creation
+### Dataset creation
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = var.dataset_id
 }
 
-resource "google_bigquery_table" "temp_table" {
+### Temporary table where the csv will be loaded
+resource "google_bigquery_table" "daily_temp_table" {
   dataset_id = google_bigquery_dataset.dataset.dataset_id
   table_id   = var.temp_table_id
 
   external_data_configuration {
     source_format = "CSV"
     autodetect    = true
-    csv_options {
-      skip_leading_rows = 1
-    }
   }
 }
 
+### Main table where consiolidated data goes
 resource "google_bigquery_table" "main_table" {
   dataset_id = google_bigquery_dataset.dataset.dataset_id
   table_id   = var.main_table_id
-
-  schema {
-    name = "field1"
-    type = "STRING"
-    mode = "NULLABLE"
-  }
+  deletion_protection = false
+  schema = file("path/to/file")
 }
 
-resource "google_bigquery_data_transfer_config" "transfer" {
-  display_name = "Your Data Transfer Config"
+# [START] Jobs for consolidation
+resource "google_bigquery_data_transfer_config" "transfer_job" {
+  display_name = "Data Transfer Job"
   data_source_id = "google_cloud_storage"
 
   params = <<PARAMS
@@ -65,7 +62,8 @@ resource "google_bigquery_data_transfer_config" "transfer" {
   "bucketName": "${google_storage_bucket.csv_bucket.name}",
   "ignoreUnknownValues": false,
   "fieldDelimiter": ",",
-  "skipLeadingRows": 1
+  
+"skipLeadingRows": 1
 }
 PARAMS
 }
