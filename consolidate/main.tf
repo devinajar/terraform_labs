fae5cb56c8844b6e889a4aeda1d349d73ebc9c72
Devinajar <jarrhack@proton.me>
2023-08-18 14:16:07 +0200

    Step by step study of the scenario - redoing the main.tf

--- consolidate/main.tf
+++ consolidate/main.tf
@@ -1,44 +1,28 @@
 provider "google" {
-  credentials = file("path")
-  project = "project_id"
-  region = "eu-west1"
+  project = var.project_id
+  region  = var.region
 }
 
-# Account Settings
-### Service account
+# Bucket creation
+### The bucket
+resource "google_storage_bucket" "csv_bucket" {
+  name     = var.bucket_name
+  location = var.region # ?? Check if this is correct
+}
+
+### Service account that will load the data into the bucket
 resource "google_service_account" "data_loader" {
-  account_id = "data-loader-service-account"
+  account_id   = "data-loader-service-account"
   display_name = "Data Loader Service Account"
 }
-### Account member
-resource "google_project_iam_member" "data_loader_binding" {
+
+### Binding of the SA with the necessary role
+resource "google_project_iam_binding" "data_loader_binding" {
   project = var.project_id
-  role = "roles/bigquery.dataEditor"
-  member = "serviceAccount:${google_service_account.data_loader.email}"
-}
-
-# Storage resources
-resource "google_storage_bucket" "csv_bucket" {
-  name = "csv-bucket"
-  location = "europe-west2"
-  uniform_bucket_level_access = true
-}
-
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "consolidation_dataset"
-}
-
-resource "google_bigquery_table" "temp_table" {
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  table_id = "temp_table"
-
-  external_data_configuration {
-    source_format = "CSV"
-    autodetect = true
-    csv_options {
-      skip_leading_rows = 1
-    }
-  }
+  role    = "roles/bigquery.dataEditor"
+  members = [
+    "serviceAccount:${google_service_account.data_loader.email}"
+  ]
 }
 
 
