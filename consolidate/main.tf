ba5bde12e1031e2ce0f7d8c8a4d1bf8c54f4bfcd
Devinajar <jarrhack@proton.me>
2023-08-21 08:52:35 +0200

    Added bucket object

--- consolidate/main.tf
+++ consolidate/main.tf
@@ -10,19 +10,7 @@ resource "google_storage_bucket" "csv_bucket" {
   location = var.region # ?? Check if this is correct
 }
 
-### Service account that will load the data into the bucket
-resource "google_service_account" "data_loader" {
-  account_id   = "data-loader-service-account"
-  display_name = "Data Loader Service Account"
+resource "google_storage_bucket_object" "csv" {
+  name = "*.csv"
+  bucket = google_storage_bucket.csv_bucket.name
 }
-
-### Binding of the SA with the necessary role
-resource "google_project_iam_binding" "data_loader_binding" {
-  project = var.project_id
-  role    = "roles/bigquery.dataEditor"
-  members = [
-    "serviceAccount:${google_service_account.data_loader.email}"
-  ]
-}
-
-
