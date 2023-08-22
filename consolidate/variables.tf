fedf10d4637f4ba680b1010e0dbb5c66ab1c82d3
Devinajar <jarrhack@proton.me>
2023-08-22 11:14:09 +0200

    Modified variables file to fit the current main.tf

---
+++ consolidate/variables.tf
@@ -0,0 +1,13 @@
+variable "project" { } # Defined in `terraform.tfvars` for security
+
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "csv_bucket_name" {
+  default = "${var.project_id}_csv_bucket"
+}
+
+variable "cf_bucket_name" {
+  default = "${var.project_id}_cf_bucket"
+}
