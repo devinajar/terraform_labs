f5ee462cb96f0f02d244d8e4f79e2e784fe7a6af
Devinajar <jarrhack@proton.me>
2023-08-23 10:33:52 +0200

    Moved sensitive variables out of variables.tf

--- consolidate/variables.tf
+++ consolidate/variables.tf
@@ -1,13 +1,14 @@
-variable "project_id" { } # Defined in `terraform.tfvars` for security
+variable "project_id" {}
+variable "credentials_file" {}
 
 variable "region" {
   default = "europe-west1"
 }
 
 variable "csv_bucket_name" {
-  default = "${var.project_id}_csv_bucket"
+  default = "labs-for-ltech_csv_bucket"
 }
 
 variable "cf_bucket_name" {
-  default = "${var.project_id}_cf_bucket"
+  default = "labs-for-ltech_cf_bucket"
 }
