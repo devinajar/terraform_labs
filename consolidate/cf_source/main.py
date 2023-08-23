1ec888772bfeadf1968356003bd33a4a25b33bc3
Devinajar <jarrhack@proton.me>
2023-08-23 13:05:11 +0200

    Functioning code for a cloud function

--- consolidate/cf_source/main.py
+++ consolidate/cf_source/main.py
@@ -1,2 +1,4 @@
-def execute_transfer_job():
-    print("Hello world")
+def execute_transfer_job(event, context):
+    print("Cloud function triggered successfully!")
+    print("Event data:", event)
+    print("Context data:", context)
