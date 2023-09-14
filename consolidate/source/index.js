6b3ca5f2e74837a5eb683d1c03326901b989462e
Devinajar <jarrhack@proton.me>
2023-09-14 13:44:38 +0200

    JS code tu manually run a Data Transfer job

--- consolidate/source/index.js
+++ consolidate/source/index.js
@@ -1,44 +1,48 @@
-// BASED ON CODE FROM: https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-csv#loading_csv_data_into_a_table
-
-// Import the GCP client libraries
 const functions = require('@google-cloud/functions-framework');
-const {BigQuery} = require('@google-cloud/bigquery');
-const {Storage} = require('@google-cloud/storage');
 
-// Initiate clients
-const bigquery = new BigQuery();
-const storage = new Storage();
+// Register a CloudEvent function with the Functions Framework
+functions.cloudEvent('startDataTransfer', cloudEvent => {  
+  'use strict';
 
-functions.cloudEvent('loadCSVFromGCS', event => {
-  // Variables for bucket and file name
-  const bucketName = event.bucket;
-  const filename = event.data.name;
+  function main() {
+    // [START bigquerydatatransfer_v1_generated_DataTransferService_StartManualTransferRuns_async]
+    
+    // Imports the Datatransfer library
+    const {DataTransferServiceClient} = require('@google-cloud/bigquery-data-transfer').v1;
 
-  // Variables for the Dataset and table where the data will be loaded
-  const datasetPath = process.env.DATASET_ID.split("/"); 
-  const datasetId = datasetPath[datasetPath.length -1];
-  const tableId = process.env.TABLE_ID;
+    // Instantiates a client
+    const dfClient = new DataTransferServiceClient();
 
-  // Data loading job configuration
-  // Omitting schema since it's already declared in the destination table
-  const metadata = {
-    sourceFormat: 'CSV',
-    skipLeadingRows: 1,
-    location: process.env.REGION,
-    // Set the write disposition to overwrite existing table data.
-    writeDisposition: 'WRITE_TRUNCATE',
-  };
-  
-  // Job to load data from a GCS into a table
-  bigquery
-  .dataset(datasetId)
-  .table(tableId)
-  .load(storage.bucket(bucketName).file(filename), metadata)
-  .then(() => {
-    // load() waits for the job to finish
-    console.log(`Loaded ${filename} to ${datasetId}.${tableId}`);
-  })
-  .catch(err =>{
-    console.log('Error loading file into BigQuery:', err);
-  })
+    /**
+     *  Transfer configuration name in the form:
+     *  `projects/{project_id}/transferConfigs/{config_id}` or
+     *  `projects/{project_id}/locations/{location_id}/transferConfigs/{config_id}`.
+     */
+    const parent = process.env.TRANSFER_ID;
+    console.log(`Parent: ${parent}`);
+
+    async function callStartManualTransferRuns() {
+      // Construct request
+      const request = {
+        parent: parent,
+        requestedRunTime: {
+          seconds: Math.floor(new Date().getTime() / 1000),
+        },
+      };
+
+      // Run request
+      const response = await dfClient.startManualTransferRuns(request);
+      console.log(response);
+    }
+
+    callStartManualTransferRuns();
+    // [END bigquerydatatransfer_v1_generated_DataTransferService_StartManualTransferRuns_async]
+  }
+
+  process.on('unhandledRejection', err => {
+    console.error(err.message);
+    process.exitCode = 1;
+  });
+  main(...process.argv.slice(2));
 });
+
